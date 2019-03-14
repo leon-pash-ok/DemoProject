c716e24250646f237ee5e4afeaaf00297929e7ca
LeonidovPavel <leonidovpavel84@gmail.com>
2019-03-14 18:57:18 +0200

    некоторые правки

--- core/src/main/java/ua/leonpasha/corp/core/domain/entities/Book.java
+++ core/src/main/java/ua/leonpasha/corp/core/domain/entities/Book.java
@@ -1,6 +1,10 @@
 package ua.leonpasha.corp.core.domain.entities;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "BOOK")
